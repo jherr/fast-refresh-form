c7b9c403ac5dfce68b0572e13c1a65e039d167b0
Jack Herrington <jherr@pobox.com>
2020-05-04 12:03:30 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Container,
+  CssBaseline,
+  makeStyles,
+  TextField,
+  Typography,
+  Button,
+} from "@material-ui/core";
+import { useForm } from "react-hook-form";
+
+const useStyles = makeStyles({
+  textField: {
+    marginTop: "1em",
+  },
+  submitBox: {
+    border: "10px dashed hotpink",
+    padding: "1em",
+    marginTop: "1em",
+    borderRadius: "2em",
+  },
+});
 
 function App() {
+  const { register, errors, formState } = useForm({
+    mode: "onChange",
+    reValidateMode: "onChange",
+    defaultValues: {
+      name: "",
+      phone: "",
+    },
+  });
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <CssBaseline />
+      <TextField
+        label="Your name"
+        fullWidth
+        variant="outlined"
+        type="text"
+        name="name"
+        className={classes.textField}
+        error={!!errors.name}
+        inputRef={register({ required: true })}
+      />
+      <TextField
+        label="Your phone"
+        fullWidth
+        variant="outlined"
+        type="text"
+        name="phone"
+        className={classes.textField}
+        error={!!errors.phone}
+        inputRef={register({
+          required: true,
+          pattern: /^\d\d\d-\d\d\d-\d\d\d\d$/,
+        })}
+      />
+      {formState.isValid && (
+        <div className={classes.submitBox}>
+          <Typography variant="h5">You are ready to go!</Typography>
+          <div style={{ textAlign: "center" }}>
+            <Button variant="contained">Submit</Button>
+          </div>
+        </div>
+      )}
+    </Container>
   );
 }
 
